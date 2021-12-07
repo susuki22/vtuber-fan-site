11c25faabfb437b50dd4a497b24a6979314c5868
susuki22 <shoya.s.0427@gmail.com>
2021-12-07 12:12:28 +0000

    投稿機能追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,28 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.all
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    @tweet.save
+    redirect_to tweets_path
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to tweets_path
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:image, :tweet_text)
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,5 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :tweet_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  attachment :image
 end
