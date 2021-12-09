60559f576d8e21a212f84385a877ed3e8b1caef4
susuki22 <shoya.s.0427@gmail.com>
2021-12-09 08:26:57 +0000

    ユーザーページ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :tweet_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  attachment :profile_image
 end
