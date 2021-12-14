784dcbb4914aa50be6f109f047732beb48da2688
susuki22 <shoya.s.0427@gmail.com>
2021-12-14 12:03:27 +0000

    secret_key追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1d6b76ae554663270f6c316898ca889009517cddfe96c79e4b1d29325c0d946249f8398d6e1ae1c927384c4b05b2791c3a8df20cfc7a345a3e8cdefe7b4fd6aa'
