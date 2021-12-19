0e737d3105f75cdc0942af38b1c3a8691f0ef283
susuki22 <shoya.s.0427@gmail.com>
2021-12-19 11:29:35 +0000

    投稿時間追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tweets = @user.tweets
+    @tweets = @user.tweets.order(created_at: :desc)
   end
 
   def edit
