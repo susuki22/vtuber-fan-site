362dc74f9ddbce3274675c876efaadf7dacecc62
susuki22 <shoya.s.0427@gmail.com>
2021-12-22 07:54:51 +0000

    レイアウト変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,11 @@ class FavoritesController < ApplicationController
     tweet = Tweet.find(params[:tweet_id])
     favorite = current_user.favorites.new(tweet_id: tweet.id)
     favorite.save
-    redirect_to tweet_path(tweet)
   end
 
   def destroy
     tweet = Tweet.find(params[:tweet_id])
     favorite = current_user.favorites.find_by(tweet_id: tweet.id)
     favorite.destroy
-    redirect_to tweet_path(tweet)
   end
 end
