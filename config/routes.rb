bde2caf196b3329a94e633b0ca8fe56549631897
susuki22 <shoya.s.0427@gmail.com>
2021-12-23 07:42:50 +0000

    フォロー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
   get 'search' => 'tweets#search'
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :tweets, only: [:index, :show, :new, :create, :destroy] do
     resources :tweet_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
